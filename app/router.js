6ca0bdc59dddccf494b0aef08a0a75a070b0e14e
Stefan Penner <stefan.penner@gmail.com>
2016-03-23 10:32:48 -0700

    updates routes

--- app/router.js
+++ app/router.js
@@ -6,6 +6,12 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('pull', { path: 'pull/:id' }, function() {
+    this.route('conversation');
+    this.route('commits');
+    this.route('files');
+    this.route('comments');
+  });
 });
 
 export default Router;
