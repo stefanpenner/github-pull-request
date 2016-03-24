10f997931718480efd92353ad60825b879c8d155
Stefan Penner <stefan.penner@gmail.com>
2016-03-24 09:48:17 -0700

    cleanup files

--- app/routes/pull/files.js
+++ app/routes/pull/files.js
@@ -4,7 +4,7 @@ import config from 'github-pull-request/config/environment';
 
 export default Route.extend({
   model() {
-    let id = this.modelFor('pull').number;
-    return ajax(`https://api.github.com/repos/ember-cli/ember-cli/pulls/${id}/files?access_token=${config.github_token}`)
+    let { number } = this.modelFor('pull');
+    return ajax(`https://api.github.com/repos/ember-cli/ember-cli/pulls/${number}/files?access_token=${config.github_token}`);
   }
 });
